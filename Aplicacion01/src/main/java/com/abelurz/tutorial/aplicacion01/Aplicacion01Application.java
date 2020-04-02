55bf81562d2d4edb88ec45370d11de74ef112819
TPCS <TPCS@10.0.0.3>
2020-04-02 18:49:33 -0400

    Commit Inicial STS

---
+++ Aplicacion01/src/main/java/com/abelurz/tutorial/aplicacion01/Aplicacion01Application.java
@@ -0,0 +1,13 @@
+package com.abelurz.tutorial.aplicacion01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplicacion01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aplicacion01Application.class, args);
+	}
+
+}
